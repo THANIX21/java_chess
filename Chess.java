897ff643dbb89af1282953b905fc7dbe3f805ce8
THANIX21 <marcohough73@gmail.com>
2020-11-26 10:38:13 +0200

    Chess board setup complete incorporating class initialization

--- Chess.java
+++ Chess.java
@@ -1,10 +1,24 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Chess {
-     public static void main(String args[]) {
-          String[][][] board = new String [8][8][100];
-          board
 
+     public static void main(String args[]) {
+
+          String[][][] board = new String [8][8][100];
+          Board ini = new Board();
+          board = ini.setup();
+          String line = new String();
+
+          for (int y = 0; y < 8; y++ ){
+
+               line = "";
+
+               for (int x = 0; x < 8; x++ ){
+
+                    line = line + board[x][y][0];
+               }
+
+               System.out.println(line);
+          }
      }
 }
--- board.java
+++ board.java
@@ -1,19 +1,22 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class board{
+public class Board{
 
-     private String[][][] setup(){
+     public String[][][] setup(){
 
           String[][][] theBoard = new String [8][8][100];
 
-          for (int x = 0; x < 8; x++ ){
+          for (int z = 0; z < 100; z++){
+
                for (int y = 0; y < 8; y++ ){
-                    theBoard[x][y][0] = ".";
+
+                    for (int x = 0; x < 8; x++ ){
+
+                         theBoard[x][y][z] = ".";
+                    }
                }
           }
 
-          /*
           theBoard[0][0][0] = "r";
           theBoard[7][0][0] = "r";
           theBoard[1][0][0] = "n";
@@ -22,9 +25,20 @@ public class board{
           theBoard[5][0][0] = "b";
           theBoard[3][0][0] = "q";
           theBoard[4][0][0] = "k";
-          */
+          theBoard[0][7][0] = "R";
+          theBoard[7][7][0] = "R";
+          theBoard[1][7][0] = "N";
+          theBoard[6][7][0] = "N";
+          theBoard[2][7][0] = "B";
+          theBoard[5][7][0] = "B";
+          theBoard[3][7][0] = "Q";
+          theBoard[4][7][0] = "K";
+
+          for (int p = 0; p < 8; p++){
+               theBoard[p][1][0] = "p";
+               theBoard[p][6][0] = "P";
+          }
 
           return theBoard;
      }
-
 }
